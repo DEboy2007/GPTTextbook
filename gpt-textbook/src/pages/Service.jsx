55da654546964fdf54fac72c19b0c33c1c4cae3d
Dhanush Ekollu <66331423+DEboy2007@users.noreply.github.com>
2023-05-10 17:45:00 -0700

    State implemented but LOTS of styling problems :(

--- gpt-textbook/src/pages/Service.jsx
+++ gpt-textbook/src/pages/Service.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import '../styles/Service.css';
+import { useState } from 'react';
 
 const Service = () => {
     // Replace with state later to give actual answer
-    let answer = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius sit amet risus a viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In a ultricies est. Nulla interdum tincidunt venenatis. Suspendisse a lectus sed odio sodales molestie. Etiam vitae leo neque. Curabitur vitae ligula eget felis mattis molestie. Donec tincidunt, nulla at consectetur aliquet, lectus dui bibendum orci, sit amet congue ante lectus sit amet tellus."
+    const [answer, setAnswer] = useState("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec varius sit amet risus a viverra. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In a ultricies est. Nulla interdum tincidunt venenatis. Suspendisse a lectus sed odio sodales molestie. Etiam vitae leo neque. Curabitur vitae ligula eget felis mattis molestie. Donec tincidunt, nulla at consectetur aliquet, lectus dui bibendum orci, sit amet congue ante lectus sit amet tellus.");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const textbook = event.target.elements.textbook.value;
+        setAnswer(textbook);
+        console.log(textbook);
+    }
+
+    const formRef = React.createRef();
+
     return (
         <div classname="container">
             <div>
@@ -12,7 +23,7 @@ const Service = () => {
             </div>
             <br />
             <div className="question">
-                <form>
+                <form ref={formRef} onSubmit={handleSubmit}>
                     <select name="textbook" id="textbook">
                         <option value="select">Textbook</option>
                         <option value="euro">AP European History</option>
@@ -25,8 +36,8 @@ const Service = () => {
                 </form>
             </div>
             <br />
-            <div className="answer">
-                <p>{answer}</p>
+            <div>
+                <textarea value={answer} readOnly={true} className='answer' />
             </div>
         </div>
     )
