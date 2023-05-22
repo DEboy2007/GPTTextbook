394c1640e335d5fbda9e11db4d52bbc1d278c39c
Dhanush Ekollu <dhanush.ekollu@gmail.com>
2023-05-22 14:26:05 -0700

    Added firestore functionality and logging

--- gpt-textbook-next/app/service/page.js
+++ gpt-textbook-next/app/service/page.js
@@ -2,11 +2,42 @@
 import React from 'react';
 import { useState } from 'react';
 import styles from './page.module.css';
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, addDoc, serverTimestamp } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const Service = () => {
     const [answer, setAnswer] = useState("Answer appears here!");
     const [question, setQuestion] = useState("No answer yet...");
     const [model, setModel] = useState("gpt-3.5-turbo");
+
+
+    async function writeData(data) {
+        try {
+            const docRef = await addDoc(collection(db, "requests"), {
+              prompt: question,
+              time: serverTimestamp(),
+              tokens: data["usage"]["total_tokens"],
+              uid: "placeholder",
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+    }
 
     async function handleGPTRequest() {
         console.log("Calling OpenAI API");
@@ -27,6 +58,7 @@ const Service = () => {
             }).then(data => {
                 console.log(data);
                 setAnswer(data["choices"][0]["message"].content);
+                writeData(data);
             });
         } catch (error) {
             console.log(error);
